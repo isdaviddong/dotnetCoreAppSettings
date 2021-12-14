6eeb6e2010cb53da21db70ddc1454afb67a977ed
twdeveloper <twdeveloper@outlook.com>
2021-12-14 22:20:46 +0800

    v1

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace testappsetting.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Enviroment = "";
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+        {
+            _logger = logger;
+            Enviroment = config.GetSection("AppConfiguration")["Enviroment"];
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
